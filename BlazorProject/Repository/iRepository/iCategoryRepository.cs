222a4ec6a0743e6fa5e8fd5d353e8e109d51af1f
Belwin.Subramonian <Belwin.Subramonian@DINCD-30281>
2025-12-03 18:21:52 +0530

    Add Category entity with migration and repository
    Introduced a new `Category` entity to the project, including:
    - Updated `ApplicationDbContext` to include `DbSet<Category>` and seed initial data.
    - Added migration to create the `Category` table and seed data.
    - Created `CategoryRepository` and `iCategoryRepository` for CRUD operations.
    - Updated `appsettings.json` to rename the database.
    - Added new folders to the project structure for better organization.

---
+++ BlazorProject/Repository/iRepository/iCategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using BlazorProject.Data;
+
+namespace BlazorProject.Repository.iRepository
+{
+    public interface iCategoryRepository
+    {
+        public Category Create(Category obj);
+        public Category Update(Category obj);
+        public bool Delete(int id);
+        public Category Get(int id);
+        public IEnumerable<Category> GetAll();
+
+    }
+}
